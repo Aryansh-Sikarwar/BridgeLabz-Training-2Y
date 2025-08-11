3d65b73fbcc8d42f5930551463f4b912ffa592c2
Aryansh-Sikarwar <sikarwararyansh@gmail.com>
2025-08-11 16:46:13 +0530

    Update HelloWorld.java

--- Fundamental-Practice-Problem/HelloWorld.java
+++ Fundamental-Practice-Problem/HelloWorld.java
@@ -1,8 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("changed file");
-        System.out.println("    hello   world   ");
-
         System.out.println("merge conflit");
     }
 }
